55eda646d2d15b1fea1c123f64958aeced9a3a43
SaniMohibur <mohiburrahmansani@gmail.com>
2026-01-31 19:12:05 +0600

    add cloudinary

--- src/app/modules/guestInfo/guestInfo.route.ts
+++ src/app/modules/guestInfo/guestInfo.route.ts
@@ -3,6 +3,7 @@ import { GuestInfoControllers } from './guestInfo.controller';
 import validateRequest from '../../middlewares/validateRequest';
 import { GuestInfoVal } from './guestInfo.validation';
 import auth from '../../middlewares/auth';
+import { fileUploader } from '../../helper/fileUploder';
 
 const router = express.Router();
 
@@ -11,6 +12,7 @@ router.get('/', GuestInfoControllers.getGuestInfo);
 router.put(
   '/',
   auth(),
+  fileUploader.upload.array('files'),
   validateRequest(GuestInfoVal.updateGuestInfo),
   GuestInfoControllers.updateGuestInfo,
 );
